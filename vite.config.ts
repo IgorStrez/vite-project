c6fee7a6d24ef856b0c14138ea717714e62c7040
IgorStrez <w5813336@gmail.com>
2024-07-22 13:33:24 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-project/'
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+      },
+    },
+  },
 })
