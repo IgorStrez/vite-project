fb9200b0ffcad8fe1675862bb63a45476bbe4e7a
IgorStrez <w5813336@gmail.com>
2024-07-25 08:09:01 +0200

    Initial commit

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -23,14 +23,14 @@ const Contacts = () => (
               <p>&nbsp;</p>
               <p>
                 Официальный сайт Сергея Мавроди: www.sergey-mavrodi.com<br /><br />
-                Магический Магазин Мавроди: www.sergeymavrodi.com<br /><br />
-                {/* Международная версия Сайта: www.sergey-mavrodi.ms<br /><br /><br /> */}
                 KinoTheater "LUZIFER" (online): www.kinotheater-luzifer.com<br /><br />
+                {/* Магический Магазин Мавроди: www.sergeymavrodi.com<br /><br /> */}
+                {/* Международная версия Сайта: www.sergey-mavrodi.ms<br /><br /><br /> */}
                 {/* Генерация Искуственного Интелекта: www.kinotheater.de<br /><br /> */}
                 Творчество Сергея Мавроди: творчество-мавроди.рф<br /><br /><br />
                 {/* Зеркало от Сайта: www.luzifer.ru<br /><br /> */}
                 {/* Личные кабинеты Сайта: www.mavrodi-mmm-2011.com<br /><br /> */}
-                АудиоКнига (от Простого Человека): sites.google.com/site/sergejmavrodisynlucifera<br /><br />
+                {/* АудиоКнига (от Простого Человека): sites.google.com/site/sergejmavrodisynlucifera<br /><br /> */}
               </p>
             </div>
             <div style={{ color: 'rgb(0, 0, 0)', fontFamily: 'Times New Roman, Times, serif', fontSize: '16px' }}>
