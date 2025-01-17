0a7c241e9c9d38cb3480fe03adedea8d4ab401ad
IgorStrez <w5813336@gmail.com>
2025-01-17 10:00:08 +0100

    Initial commit

--- src/components/Gedichte/Gedicht493.tsx
+++ src/components/Gedichte/Gedicht493.tsx
@@ -5,7 +5,7 @@ const Gedicht493 = () => (
     <div className="breadcrumbs">
       <div id="breadcrumb">
       <Link to="/">Главная страница</Link> → <Link to="/schriftstellerei">Творчество</Link> → <Link to="/gedichte">
-      Мои стихи</Link> → Из цикла Страшный суд. Ада бездна — ужасна!..
+      Мои стихи</Link> → Из цикла Страшный суд. Слова — бред!..
       </div>
     </div>
     <div className="content">
@@ -18,32 +18,24 @@ const Gedicht493 = () => (
               <div style={{backgroundColor: 'rgb(255, 255, 255)',}}>
 
 
-                <h2>Из цикла Страшный суд. <br />Ада бездна — ужасна!..</h2>
+                <h2>Из цикла Страшный суд. <br />Слова — бред!..</h2>
                 <br />
                 
                 <br />
 
                 <div className="entry-content">
-			<p style={{textAlign: 'center'}}>Ада бездна –<br />
-ужасна!<br />
-Бесполезна,<br />
-напрасна<br />
-Мольба.</p>
-<p style={{textAlign: 'center'}}>Об отмене,<br />
-отсрочке,<br />
-О замене…<br />
-До точки<br />
-Судьба</p>
-<p style={{textAlign: 'center'}}>Дочитает<br />
-сурово,<br />
-Полистает,<br />
-и снова:<br />
-«Итак!</p>
-<p style={{textAlign: 'center'}}>Говори,<br />
-коли хочешь.<br />
-Да не ври!<br />
-Что бормочешь?<br />
-Ну, так!..»</p>
+			<p style={{textAlign: 'center'}}>Слова – бред!<br />
+Даже не бред.<br />
+Вздор!</p>
+<p style={{textAlign: 'center'}}>Дела? Чушь!<br />
+Даже не чушь.<br />
+Сор.</p>
+<p style={{textAlign: 'center'}}>И всем нам<br />
+Не по делам,<br />
+Нет.</p>
+<p style={{textAlign: 'center'}}>А по – мечтам!<br />
+Сколько-то там<br />
+Лет.</p>
 		</div>
 
               </div>
--- src/components/Gedichte/Gedicht506.tsx
+++ src/components/Gedichte/Gedicht506.tsx
@@ -5,7 +5,7 @@ const Gedicht506 = () => (
     <div className="breadcrumbs">
       <div id="breadcrumb">
       <Link to="/">Главная страница</Link> → <Link to="/schriftstellerei">Творчество</Link> → <Link to="/gedichte">
-      Мои стихи</Link> → Из цикла Тюрьма, Эх, жёны, жёны у заключённых!..
+      Мои стихи</Link> → Из цикла Тюрьма, Здесь уходят навек…
       </div>
     </div>
     <div className="content">
@@ -18,30 +18,20 @@ const Gedicht506 = () => (
               <div style={{backgroundColor: 'rgb(255, 255, 255)',}}>
 
 
-                <h2>Из цикла Тюрьма, <br />Эх, жёны, жёны у заключённых!..</h2>
+                <h2>Из цикла Тюрьма, <br />Здесь уходят навек…</h2>
                 <br />
                 
                 <br />
 
                 <div className="entry-content">
-			<p style={{textAlign: 'center'}}>Эх, жёны, жёны<br />
-У заключённых!<br />
-Тоской сожжёны<br />
-Ночей бессонных.</p>
-<p style={{textAlign: 'center'}}>Всё в переплавку! Любовь и честь.<br />
-Ведь бед – не счесть.</p>
-<p style={{textAlign: 'center'}}>Ведь мук – без меры.<br />
-А жить-то надо!<br />
-И нет уж веры.<br />
-Ну, а награда –</p>
-<p style={{textAlign: 'center'}}>Ещё когда-то!.. И будет ли?<br />
-И не моли</p>
-<p style={{textAlign: 'center'}}>Судьбу напрасно.<br />
-Ведь – бесполезно.<br />
-«Да всё прекрасно!»<br />
-Но если честно –</p>
-<p style={{textAlign: 'center'}}>Уж всё сгорело. Какая «честь»?!..<br />
-Ведь бед – не счесть.</p>
+			<p style={{textAlign: 'center'}}>Здесь уходят навек,<br />
+Говоря: «До свиданья!»<br />
+Уж таков человек:<br />
+Сожаленья, рыданья –</p>
+<p style={{textAlign: 'center'}}>Большей частью лишь маска.<br />
+Лицедейство. И только.<br />
+И прощальная ласка<br />
+Не стоит – нисколько.</p>
 		</div>
 
               </div>
