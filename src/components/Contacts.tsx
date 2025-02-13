6ccb797544d9087d8a6c3e5c90dd6121f920aa03
IgorStrez <w5813336@gmail.com>
2025-02-13 23:22:00 +0100

    Монетка3

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import Coin from './Coin'; 
 import './Contacts.css';
 
 const Contacts = () => (
@@ -15,17 +14,31 @@ const Contacts = () => (
         <div className="cwc_mid">
           <div className="content">
             <h2>КОНТАКТЫ</h2>
-            <br /><hr></hr> <br />
             <div style={{ backgroundColor: 'white' }}>
-              
-              {/* МОНЕТКА */}
-              <Coin />
+              <p><br /><br /><br />
+                Заявления и предложения, поздравления и подарки, жалобы и проклятия - принимаются в адрес:
+                <span style={{ color: 'rgb(236, 36, 36)' }}> theatermavrodi@gmail.com</span>
+              </p>
 
+              <p>&nbsp;</p>
+              <p>
+                Официальный сайт Сергея Мавроди: www.sergey-mavrodi.com<br /><br />
+                KinoTheater "LUZIFER" (online): www.kinotheater-luzifer.com<br /><br /><br />
+                {/* Магический Магазин Мавроди: www.sergeymavrodi.com<br /><br /> */}
+                {/* Международная версия Сайта: www.sergey-mavrodi.ms<br /><br /><br /> */}
+                {/* Генерация Искуственного Интелекта: www.kinotheater.de<br /><br /> */}
+                Зеркала сайта Сергея Мавроди:<br /><br />
+                www.mavrodi-mmm-2011.com<br /><br />
+                www.sergey-mavrodi.ms
+                {/* Зеркало от Сайта: www.luzifer.ru<br /><br /> */}
+                {/* Личные кабинеты Сайта: www.mavrodi-mmm-2011.com<br /><br /> */}
+              </p>
             </div>
             <div style={{ color: 'rgb(0, 0, 0)', fontFamily: 'Times New Roman, Times, serif', fontSize: '16px' }}>
-              <br/>
+              &nbsp;
             </div>
             <div style={{ color: 'rgb(0, 0, 0)', fontFamily: 'Times New Roman, Times, serif', fontSize: '16px' }}>
+              &nbsp;
             </div>
           </div>
         </div>
